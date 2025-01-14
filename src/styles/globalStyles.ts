47aafa0f25c98fe1c2e591f79d61c9ee2a7a2bc1
Anton <a.chertok@akatech.com.ua>
2025-01-14 19:20:30 +0000

    init

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,69 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *, *::before, *::after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    html{ 
+        scroll-behavior: smooth;
+    }
+
+    body {
+        max-width: 100vw;
+        overflow-x: hidden;
+        margin: 0;
+        padding: 0;
+        font-size: 16px;
+        line-height: 1.17;
+        color: #000000;
+        background-color: #ffffff;
+        font-family: "Roboto", sans-serif;
+        font-style: normal;
+        font-weight: 400;
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+    }
+    
+    li {
+        list-style-type: none;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p,
+    li {
+        margin: 0;
+        padding: 0;
+    }
+
+    input {
+        margin: 0;
+        padding: 0;
+        background: transparent;
+        border: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button {
+        cursor: pointer;
+        margin: 0;
+        padding: 0;
+        background-color: transparent;
+        border: none;
+    }
+`;
+
+export default GlobalStyle;
